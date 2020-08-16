ae494291cd78c70ddc622f39d0b324cc9cb44bdc
Jafar Husain <jhusain@gmail.com>
2020-08-16 09:21:32 -0700

    Added more logs

--- ChromeExtension/content.js
+++ ChromeExtension/content.js
@@ -1,9 +1,13 @@
 console.log("CONTENT SCRIPT RUN");
 var script = document.createElement("script");
 script.text = `
-if (document.location.href.includes('?voice_search=true')) {
-  document.querySelector("div[aria-label='Search by voice']").click();
-}
+console.log('INJECTED SCRIPT RUN');
+document.addEventListener('DOMContentLoaded', (event) => {
+  console.log('DOCUMENT SCRIPT RUN');
+  if (document.location.href.includes('?voice_search=true')) {
+    document.querySelector("div[aria-label='Search by voice']").click();
+  }
+});
 `;
 
 (document.head || document.documentElement).appendChild(script);
