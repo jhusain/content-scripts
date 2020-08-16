abc21e9ca1ef7ee814095cd168df700fe84c4f0d
Jafar Husain <jhusain@gmail.com>
2020-08-16 09:13:30 -0700

    Adding logs

--- ChromeExtension/content.js
+++ ChromeExtension/content.js
@@ -1,3 +1,9 @@
+console.log("CONTENT SCRIPT RUN");
 var script = document.createElement("script");
-script.text = `document.querySelector("div[aria-label='Search by voice']").click();`;
+script.text = `
+if (document.location.href.includes('?voice_search=true')) {
+  document.querySelector("div[aria-label='Search by voice']").click();
+}
+`;
+
 (document.head || document.documentElement).appendChild(script);
