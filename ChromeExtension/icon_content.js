5ece8fb53d100acaa693dcd69fc48a713197d210
Jafar Husain <jhusain@gmail.com>
2020-08-16 10:33:19 -0700

    Icon bug fix

--- ChromeExtension/icon_content.js
+++ ChromeExtension/icon_content.js
@@ -2,9 +2,9 @@ console.log("CONTENT SCRIPT RUN");
 var script = document.createElement("script");
 const logoUrl = chrome.extension.getURL("logo.png");
 script.text = `
-const navButton = document.createElement("div");
-navButton.style="position: fixed; bottom:0px; right:0px";
-navButton.innerHTML = "<a href='https://www.google.com?voice_search=true' target='_blank'><img width='200' height='200' src='${logoUrl}'></a>";
-document.appendChild(navButton);
+const flerg = document.createElement("div");
+flerg.style="position: fixed; bottom:0px; right:0px";
+flerg.innerHTML = "<a href='https://www.google.com?voice_search=true' target='_blank'><img width='150' height='150' src='${logoUrl}'></a>";
+document.documentElement.appendChild(flerg);
 `;
 (document.head || document.documentElement).appendChild(script);
