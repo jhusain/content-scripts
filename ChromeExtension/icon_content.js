bee826240149e85e49c07b6b81df9c84fc0bfef5
Jafar Husain <jhusain@gmail.com>
2020-08-16 11:05:51 -0700

    fixed url

--- ChromeExtension/icon_content.js
+++ ChromeExtension/icon_content.js
@@ -1,12 +1,11 @@
 console.log("CONTENT SCRIPT RUN");
 var script = document.createElement("script");
 const logoUrl = chrome.extension.getURL("logo.svg");
-const size = 150;
+const size = 50;
 script.text = `
 const flerg = document.createElement("div");
 flerg.style="position: fixed; bottom:0px; right:0px";
-flerg.innerHTML = "<a href='${logoUrl}' target='_blank'><img width='${size}' height='${size}' src='${logoUrl}'></a>";
+flerg.innerHTML = "<a href='https://www.google.com/?voice_search=true' target='_blank'><img width='${size}' height='${size}' src='${logoUrl}'></a>";
 document.documentElement.appendChild(flerg);
-document.documentElement.style['margin-bottom']= '${size}px';
 `;
 (document.head || document.documentElement).appendChild(script);
