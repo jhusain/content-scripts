05b65f448e609e2e0178657bb9d6562e86a8f97c
Jafar Husain <jhusain@gmail.com>
2020-08-16 09:41:42 -0700

    removed document ready

--- ChromeExtension/content.js
+++ ChromeExtension/content.js
@@ -2,13 +2,10 @@ console.log("CONTENT SCRIPT RUN");
 var script = document.createElement("script");
 script.text = `
 console.log('INJECTED SCRIPT RUN');
-document.addEventListener('DOMContentLoaded', (event) => {
-  console.log('DOCUMENT SCRIPT RUN');
-  if (document.location.href.includes('?voice_search=true')) {
-    console.log('VOICE SEARCH RUN');
-    document.querySelector("div[aria-label='Search by voice']").click();
-  }
-});
+if (document.location.href.includes('?voice_search=true')) {
+  console.log('VOICE SEARCH RUN');
+  document.querySelector("div[aria-label='Search by voice']").click();
+}
 `;
 
 (document.head || document.documentElement).appendChild(script);
