c295fea116c5b2684bb267aaef929d599fa10ec4
Jafar Husain <jhusain@gmail.com>
2020-08-16 11:18:03 -0700

    upped z-index

--- ChromeExtension/icon_content.js
+++ ChromeExtension/icon_content.js
@@ -4,7 +4,8 @@ const logoUrl = chrome.extension.getURL("logo.svg");
 const size = 50;
 script.text = `
 const flerg = document.createElement("div");
-flerg.style="position: fixed; bottom:0px; right:0px";
+flerg.style="position: fixed; bottom:0px; right:0px;z-index:2147483647;
+ ";
 flerg.innerHTML = "<a href='https://www.google.com/?voice_search=true' target='_blank'><img width='${size}' height='${size}' src='${logoUrl}'></a>";
 document.documentElement.appendChild(flerg);
 `;
